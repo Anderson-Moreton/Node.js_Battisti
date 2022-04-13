3ed648cf66f2d534b8192f1bdc01145c05f112b5
Anderson Moreton <anderson.moreton@gmail.com>
2022-04-13 16:03:50 -0300

    Curso de Node.JS

---
+++ 2_Fundamentos/7_explorando_console/index.js
@@ -0,0 +1,18 @@
+//Mais de um valor
+
+const x = 10;
+const y = "Anderson";
+const z = [1, 6]
+
+console.log(x, y, z)
+
+//Contagem de Impressões
+console.log(`O valor de x é: ${x}, contagem`)
+
+//Variavel entre string
+console.log("O nome é %s, e ele é programador", y)
+
+//Limpar o console
+setTimeout(() => {
+    console.clear()
+}, 2000)
